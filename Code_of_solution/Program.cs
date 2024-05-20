91a43d1f4dc67aef4092c73b44ed3931c32e5725
Alexander Pisminskiy <frosya0@bk.com>
2024-05-20 19:11:27 +0300

    Создал папку под решение и иницировал в папке консоль

---
+++ Code_of_solution/Program.cs
@@ -0,0 +1,29 @@
+﻿string[] array1 = new string[5] {"q", "qw", "qwe", "qwer", "qwert"};
+string[] array2 = new string[array1.Length];
+
+void FillingArray2(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+
+void PrintResult(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+FillingArray2(array1, array2);
+PrintResult(array2);
